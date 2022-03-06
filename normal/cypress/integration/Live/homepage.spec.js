2929d68f11555ec606ec80c180743a7b0660934f
Usman <usman.solemn@gmail.com>
2022-03-06 22:33:56 +0500

    updates

--- normal/cypress/integration/Live/homepage.spec.js
+++ normal/cypress/integration/Live/homepage.spec.js
@@ -3,8 +3,18 @@ describe('Home page of www.usmanrafiq.com', () => {
      cy.visit('https://www.usmanrafiq.com')
   })
 
-  it('displays two todo items by default', () => {
+  it('website has heading available', () => {
     cy.get('h1').first().should('have.text', '\nM.Usman Rafiq | Programming Escapades\n')
   });
 
+  it('website has sub-heading available', () => {
+    cy.get('p').first().should('have.text', 'Things I have learned, experienced and explored')
+  });
+
+  it('website has menu items', () => {
+    cy.get('div.PageList > div > ul > li:nth-child(1) > a').contains('Home')
+
+    //.should('have.attr', 'href').and('include', 'contact')
+
+  });
 });
